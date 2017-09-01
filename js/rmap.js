ba3ef03303fdf6e60e042aa6ac67af9eb0d4ad25
hui <huiran@gmail.com>
2017-08-31 23:59:50 -0700

    add filter functionality

--- js/rmap.js
+++ js/rmap.js
@@ -1,6 +1,6 @@
 "use strict";
 (function(_w, _d, $){
-	var _m, _i=[], _h, _u;
+	var _m, _h, _u, _i, _r, _a, _n;
 	var rmap = {
 		loading : function(on, msg){
 			if(on){
@@ -18,44 +18,47 @@
 		  });
 		  _t.legend(); 
 		  _m.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend'));
-		  var _h, t=0, _g = new google.maps.Geocoder();
+		  var h, t=0, g = new google.maps.Geocoder();
 			$(d.values).each(function(k,v){
 				if(k){
-					_t.sleep(500*k).then(function(){_t.pin(_g, _h, v)});
+					_t.sleep(_r*t).then(function(){_t.pin(g, h, v)});
 					t+=1;
 				}else{
-					_h = v; 
+					h = v; 
 				}
 			});
-			_t.sleep(500*t).then(()=>_t.loading(false));
+			_t.sleep(_r*t).then(function(){
+				_t.filters();
+				_t.loading(false);
+			});
 		},
 		sleep: function(t){
   		return new Promise(function(r){setTimeout(r, t)});
 		},
-		pin : function(_g, _h, v){ 
-			var a = v[_h.indexOf('Address')], t=v[0], _t=this;
+		pin : function(g, h, v){ 
+			var a = v[h.indexOf('Address')], t=v[0], _t=this;
 			if(a){
 				if(a.search(/san francisco/i) < 0){
 					a += ', San Francisco'; 
 				}
-				_g.geocode( { 'address': a}, function(r, s){
+				g.geocode( { 'address': a}, function(r, s){
 		      if (s == google.maps.GeocoderStatus.OK) {
-		        var marker = new google.maps.Marker({
+		        var m = new google.maps.Marker({
 		        		title: t,
 		            map: _m,
-		            icon: {url:_t.fuzzy(v,_h),scaledSize:new google.maps.Size(25, 25)},
+		            icon: {url:_t.fuzzy(v,h),scaledSize:new google.maps.Size(25, 25)},
 		            position: r[0].geometry.location
 		        });
 		        var w = new google.maps.InfoWindow({
-		          content: _t.content(v, _h)
+		          content: _t.content(v, h)
 		        });
-		        marker.addListener('click', function() {
-		        	for(var i=0;i<_i.length;i++){ _i[i].close(); }
-		          w.open(_m, marker);
+		        m.addListener('click', function() {
+		        	for(var i=0;i<_i.length;i++){ _i[i][1].close(); }
+		          w.open(_m, m);
 		        });
-		        marker.setAnimation(google.maps.Animation.BOUNCE);
-		        _t.sleep(500).then(function(){marker.setAnimation(null);});
-		        _i.push(w);
+		        m.setAnimation(google.maps.Animation.BOUNCE);
+		        _t.sleep(_r).then(function(){m.setAnimation(null);});
+		        _i.push([m,w]);
 		      } else {
 		        _t.error('geo: '+s+'('+t+')');
 		      }
@@ -64,16 +67,10 @@
 				console.log(t+' NG');
 			}
 		},
-		fuzzy : function(v, _h){
-			var e=this.emoji('Lunch'),a=['Lunch','Dinner','Breakfast','AM Coffee','PM Coffee','Drinks'];
-			if((new Date()).getHours()>14){
-				var x=0,y=1,n=a[x];
-				a[x]=a[y]; a[y]=n;
-				x=3,y=4,n=n=a[x];
-				a[x]=a[y]; a[y]=n;
-			}
+		fuzzy : function(v, h){
+			var e=this.emoji('Lunch'),a=_a[2];
 			for(var i=0;i<a.length;i++){
-				if(v[_h.indexOf(a[i])]){
+				if(v[h.indexOf(a[i])]){
 					e=this.emoji(a[i]);
 					break;
 				}
@@ -81,8 +78,7 @@
 			return 'css/emoji/'+e+'.png';
 		},
 		emoji : function(t){
-			var n=['Breakfast','AM Coffee','Lunch','PM Coffee','Dinner','Drinks','Good for Groups','Takes Reservations'],
-		 	i=['donut','coffee','hamburger','tea','curry','beer','busts_in_silhouette','clipboard'];
+			var n=_a[0],i=_a[1];
 		 	return  (n.indexOf(t)>-1)?i[n.indexOf(t)]:'';
 		},
 		icon : function(t){
@@ -90,13 +86,48 @@
 		 	return (e)?'<i class="em em-'+e+'"></i>':'';
 		},
 		legend : function(){
-			var l =' <div id="legend">', 
-		 	n=['Breakfast','AM Coffee','Lunch','PM Coffee','Dinner','Drinks','Good for Groups','Takes Reservations']
+			var l =' <div id="legend">', n=_a[0];
 		 	for(var i=0;i<n.length;i++){
-		 		l+='<div>'+this.icon(n[i])+' '+n[i]+'</div>';
+		 		l+='<div><a class="filter" data-value="'+_a[1][i]+'"><i class="em em-white_medium_square"></i>  '+this.icon(n[i])+' '+n[i]+'</a></div>';
 		 	}
 		 	l+='</div>';
 		 	$('body').append(l);
+		},
+		filters : function(){
+			var o = 'em-white_square_button', _t=this;
+			$('#legend .filter i:first-child').addClass(o);
+			$('#legend .filter').click(function(){_t.filter($(this),o);});
+			$('#legend').addClass('loaded');
+		},
+		filter : function(e, o){
+			var a=e.find('i:first-child'), b=e.data('value');
+			if(a.hasClass(o)){
+				_a[4].splice(_a[4].indexOf(b),1);
+				a.removeClass(o); 
+			}else{
+				a.addClass(o); _a[4].push(b);
+			}
+			for(var i=0;i<_i.length;i++){ 
+				var m=_i[i][0], w=_i[i][1], c=w.getContent(), h=true;
+				if($('#legend .filter .em-white_medium_square').length != $('#legend .filter .'+o).length){
+					for(var j=0;j<_a[4].length;j++){
+						if(c.indexOf('em-'+_a[4][j])>-1){
+							h=false; break;
+						}	
+					}
+				}else{
+					h=false;
+				}
+				if(!h){
+					m.setVisible(true);
+					for(var j=0;j<_a[3].length;j++){
+						if(_a[4].indexOf(_a[3][j]) > -1 && c.indexOf('em-'+_a[3][j])>-1){
+							m.setIcon({url:'css/emoji/'+_a[3][j]+'.png',scaledSize:new google.maps.Size(25, 25)});
+							break;
+						}
+					}
+				}else{m.setVisible(false);}
+			}
 		},
 		content : function(v, _h){
 			var h = '<div class="card">', d=['Restaurant','Local','Landmark','Address', 'Notes', 'Hours'], m=''; 
@@ -116,7 +147,7 @@
 					m+=(v[i])?this.icon(_h[i])+' ':'';
 				}
 			}
-			h += '<div>'+m+'</div></div>';
+			h += '<div class="icons">'+m+'</div></div>';
 			return h;
 		},
 		param : function(name){
@@ -133,7 +164,17 @@
 			this.loading(true, 'Something broke...');
 		},
 		initMap : function(){ 
-			var _t = this;
+			var _t = this, a;
+			a=_a[2];
+			if((new Date()).getHours()>14){
+				var x=0,y=1,n=a[x];
+				a[x]=a[y]; a[y]=n;
+				x=3,y=4,n=n=a[x];
+				a[x]=a[y]; a[y]=n;
+			} _a[2] = a;
+			for(var i=0;i<_a[2].length;i++){
+				_a[3].push(_a[1][_a[0].indexOf(_a[2][i])]);
+			}		
 			_t.loading(true);
 		  $.getJSON('https://sheets.googleapis.com/v4/spreadsheets/'+_u+'/values/Sheet1!A1:Z?key='+_h).done(function(d){_t.load(_t,d)}).fail(function(j,t,e){_t.error(t+' '+e)});
 		},
@@ -145,6 +186,12 @@
 			var _t = this;
 			_h = _t.param('h'); 
 			_u = _t.param('u');
+			_i = []; 
+			_r = 600;
+			_a = [['Breakfast','AM Coffee','Lunch','PM Coffee','Dinner','Drinks','Good for Groups','Takes Reservations'],
+						['donut','coffee','hamburger','tea','curry','beer','busts_in_silhouette','clipboard'],
+						['Lunch','Dinner','Breakfast','AM Coffee','PM Coffee','Drinks','Good for Groups','Takes Reservations'],[]]; _a[4] = _a[1]; 
+
 			_w.rmap = _t;  
 		}
 	}
